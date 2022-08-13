92d93aa3e88a90b9c28887dd34fc7f0f6cdefdbb
Paola Kossa <paolakossa@gmail.com>
2022-08-13 18:42:38 -0300

    Terceiro módulo do curso

---
+++ console.js
@@ -0,0 +1,8 @@
+//log => registro 
+const minhaVar = true;
+//console.log(245);
+//console.log ("eu sou um texto");
+//console.log(minhaVar);
+
+//tratamento de erro!
+console.error(new Error("deu erro"));
