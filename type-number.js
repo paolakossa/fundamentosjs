90dd8039303d2c1145e0b6446d85b0da6e6b723c
Paola Kossa <paolakossa@gmail.com>
2022-08-13 13:45:36 -0300

    Primeiro módulo do curso

---
+++ type-number.js
@@ -0,0 +1,25 @@
+//tipo Number
+
+const meuNumero=3;
+
+const primeiroNumero=1;
+
+const segundoNumero=2; 
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(operacaoMatematica);
+
+//ponto flutuante
+
+const pontoNumeroFlutuante=3.3;
+const prontoFlutuanteSemZero= .5;
+
+const novaOperacao= primeiroNumero / pontoNumeroFlutuante; 
+console.log(novaOperacao);
+
+//Nan (not a number)
+
+const alura = "Alura";
+
+console.log(primeiroNumero*alura);
