4b8468d2a137e7c656cc4064c4a2f4912652eb92
Paola Kossa <paolakossa@gmail.com>
2022-08-13 17:17:01 -0300

    Segunda parte do curso

---
+++ script.js
@@ -0,0 +1,34 @@
+//var altura = 5; 
+//var comprimento = 7; 
+
+//area = altura * comprimento 
+
+//console.log(area); 
+
+//var area; 
+
+//let altura = 5;
+//let comprimento = 7; 
+//let forma = "retângulo";
+//let area; 
+
+//if( forma === "retângulo"){
+    //area = altura * comprimento;
+//} else {
+    //area = (altura * comprimento)/2;
+//}
+//console.log(area);
+
+const altura = 5;
+const comprimento = 7; 
+const forma = "triangulo";
+let area; 
+
+if(forma==="quadrado"){
+    area = altura*comprimento;
+}
+else{
+    area = (altura*comprimento)/2; 
+}
+
+console.log(area); 
